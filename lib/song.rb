f5f43d9c2f53205aa8a1e4524ab5c6bbdea1bf61
Stacy Carr <stacy@boostersinabox.com>
2018-09-11 21:32:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,6 +37,17 @@ class Song
     artist_count
   end
   
-    
+  def genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1 
+      end
+    end
+    genre_count
+  end
+
 end
 
