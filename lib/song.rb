97722355e3a2bb832245d6d2f1376d8f396eba9d
Stacy Carr <stacy@boostersinabox.com>
2018-09-11 21:20:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,10 @@ class Song
     @artist = artist
     @genre = genre
     @@count +=1
-    
+    @@genres << genre
+    @@artists << artist
   end
+  
   attr_accessor :name, :artist, :genre
   
     @@count = 0 
